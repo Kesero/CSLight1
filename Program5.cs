5e0e5af62a65b8d4772aa683375095e37c288c22
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-08 21:17:39 +0100

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSLight1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float productCristallPrice = 5.6f;
+            Console.WriteLine("Добро пожаловать в магазин покупки кристаллов!");
+            Console.Write("Введите кол-во монет персонажа: ");
+            float playerCountCoins = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Цена кристаллов: " + productCristallPrice + "/1шт. " + " Сколько кристаллов вы хотите купить: ");
+            int playerCountCristall = Convert.ToInt32(Console.ReadLine());
+            playerCountCoins -= playerCountCristall * productCristallPrice;
+            Console.WriteLine($"Кол-во купленных кристаллов: {playerCountCristall}  Баланс: {playerCountCoins}");
+            Console.ReadKey();
+        }
+    }
+}
