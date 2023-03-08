13d8a7db76abf929acbe3fa15714d9db4c4dbf7d
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-08 21:33:12 +0100

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeServiceMinutes = 10;
+            Console.Write("Сколько человек в очереди: ");
+            int countPatients = Convert.ToInt32(Console.ReadLine());
+            int countMinutesPerHour = 60;
+            int countTotalWaitingMinutes = countPatients * timeServiceMinutes;
+            int timeWaitingHours = countTotalWaitingMinutes / countMinutesPerHour;
+            int timeWaitingMinutes = countTotalWaitingMinutes % countMinutesPerHour;
+            Console.WriteLine($"Вы должны отстоять в очереди {timeWaitingHours} часа и {timeWaitingMinutes} минут.");
+            Console.ReadKey();
+        }
+    }
+}
