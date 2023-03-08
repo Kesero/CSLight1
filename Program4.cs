20a7d409850734b91f68de84853a4b86e729755d
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-08 21:02:06 +0100

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientName = "Ivanov";
+            string clientSurname = "Ivan";
+            Console.WriteLine("Имя клиента до перестановки: " +  clientName);
+            Console.WriteLine("Фамилия клиента до перестановки: " + clientSurname);
+            Console.WriteLine("Перестановка...");
+            string bufferVariablePermutation = clientName;
+            clientName = clientSurname;
+            clientSurname = bufferVariablePermutation;
+            Console.WriteLine("Имя клиента после перестановки: " + clientName);
+            Console.WriteLine("Фамилия клиента после перестановки: " + clientSurname);
+            Console.ReadKey();
+        }
+    }
+}
