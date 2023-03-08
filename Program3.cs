886d2d32c7f12e6ce94bfb0c0306717301212256
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-08 20:52:34 +0100

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPictures = 52;
+            int countPicturesInRow = 3;
+            int countFilledRows = countPictures / countPicturesInRow;
+            int countPicturesOutFilledRow = countPictures % countPicturesInRow;
+            Console.WriteLine("Кол-во полность заполненных картинками рядов: " + countFilledRows);
+            Console.WriteLine("Кол-во картинок сверх меры: " + countPicturesOutFilledRow);
+            Console.ReadKey();
+        }
+    }
+}
