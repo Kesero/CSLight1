ae1d2da06f86dc9697dd671a2fecc1139d006e14
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-09 20:07:40 +0100

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSLight1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Какое сообщение вы хотите зациклить: ");
+            string clientText = Console.ReadLine();
+            Console.Write("Какие кол-во раз хотите повторить ваше сообщение: ");
+            int clientCountRepetitions = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < clientCountRepetitions; i++)
+            {
+                Console.WriteLine(clientText);
+            }
+            Console.ReadKey();
+        }
+    }
+}
