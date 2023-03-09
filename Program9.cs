6dba56c9b746d4ea1030326bf0089afe6d5ab032
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-09 20:31:57 +0100

    Add files via upload

---
+++ Program9.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSLight1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int actualNumberSequence = 5;
+            int progressionNumber = 7;
+            while (actualNumberSequence < 100)
+            {
+                Console.Write(actualNumberSequence + " ");
+                actualNumberSequence += progressionNumber;
+            }
+            Console.ReadKey();
+        }
+    }
+}
