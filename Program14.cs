2b014841ded53a397fb0b92c011f4c3753e6e597
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-13 22:04:03 +0100

    Add files via upload

---
+++ Program14.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CSLight1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string passwordInput;
+            string password = "1234";
+            int attemptСounter = 0;
+            int maxCountAttempts = 3;
+
+            while (attemptСounter < maxCountAttempts)
+            {
+                attemptСounter++;
+                Console.Write("Введите пароль, чтобы увидеть секретное сообщение: ");
+                passwordInput = Console.ReadLine();
+
+                if (passwordInput == password)
+                {
+                    Console.WriteLine("*секретное сообщение*");
+                    Console.ReadKey();
+                    break;
+                } 
+                else
+                {
+                    int currentCountAttempts = maxCountAttempts - attemptСounter;
+                    Console.WriteLine("Неверный пароль! Осталось попыток: " + currentCountAttempts);
+                }
+            }
+        }
+    }
+}
