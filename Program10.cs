71f77e2e91eb727aea09d054463042610a5b7642
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-13 20:43:45 +0100

    Add files via upload

--- Program10.cs
+++ Program10.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSLight1
 {
@@ -7,16 +8,22 @@ namespace CSLight1
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int randomLimit = rand.Next(101);
+            int limitRandom = 101;
+            int numberDivisionCondition1 = 3;
+            int numberDivisionCondition2 = 5;
+            int randomLimit = rand.Next(limitRandom);
             Console.WriteLine("Лимит: " + randomLimit);
             int amount = 0;
-            for (int i = 0; i <= randomLimit; i++) {
-                if ((i % 3 == 0) || (i % 5 == 0))
+
+            for (int i = 0; i <= randomLimit; i++)
+            {
+                if ((i % numberDivisionCondition1 == 0) || (i % numberDivisionCondition2 == 0))
                 {
                     amount += i;
                     Console.Write(i + " ");
                 }
             }
+
             Console.WriteLine();
             Console.WriteLine("Cумма положительных чисел, делящихся на 3 и 5: " + amount);
             Console.ReadKey();
