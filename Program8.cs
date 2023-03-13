fca7dc0e5ca2321ed7ff676b6fef9ac19579db77
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-13 20:27:26 +0100

    Add files via upload

--- Program8.cs
+++ Program8.cs
@@ -6,13 +6,15 @@ namespace CSLight1
     {
         static void Main(string[] args)
         {
-            string wordCompletion = "exit";
+            string wordToExit = "exit";
             string clientWord = "";
-            while (clientWord != wordCompletion)
+
+            while (clientWord != wordToExit)
             {
                 Console.Write("Введите специальное слово для выхода: ");
                 clientWord = Console.ReadLine();
             }
+
             Console.WriteLine("Вы вышли из цикла!");
             Console.ReadKey();
         }
