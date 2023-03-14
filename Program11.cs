05e36273dd6db886df4ae3dcecc346d2f1d65539
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-14 18:52:59 +0100

    Add files via upload

--- Program11.cs
+++ Program11.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CSLight1
 {
@@ -7,20 +10,22 @@ namespace CSLight1
     {
         static void Main(string[] args)
         {
-            string wordToExit = "exit";
-            string clientInputCompletion = "";
-            string clientInput1 = "";
-            string clientInput2 = "";
-            int countСonversionsCurrency;
+            const string CommandConversionCurrencyUSD = "USD";
+            const string CommandConversionCurrencyEUR = "EUR";
+            const string CommandConversionCurrencyRUB = "RUB";
+
             float rubToUsd = 75.0f;
             float usdToRub = 0.0133f;
             float rubToEur = 80.0f;
             float eurToRub = 0.0125f;
             float usdToEur = 0.9375f;
             float eurToUsd = 1.0666f;
-            const string nameCurrencyUSD = "USD";
-            const string nameCurrencyEUR = "EUR";
-            const string nameCurrencyRUB = "RUB";
+            string wordToExit = "exit";
+            string clientInputCompletion = "";
+            string clientInput1 = "";
+            string clientInput2 = "";
+            int countСonversionsCurrency;
+            
             Console.Write("Сколько у вас RUB: ");
             float clientRub = Convert.ToSingle(Console.ReadLine());
             Console.Write("Сколько у вас EUR: ");
@@ -30,91 +35,161 @@ namespace CSLight1
 
             while (clientInputCompletion != wordToExit)
             {
-                Console.WriteLine("Для выхода введите - exit. Или нажмите Enter.");
-                clientInputCompletion = Console.ReadLine();
+                Console.Write("Какую валюту вы хотите обменять (RUB, EUR, USD): ");
+                clientInput1 = Console.ReadLine();
+                Console.Write("В какую валюту хотите сделать обмен: ");
+                clientInput2 = Console.ReadLine();
 
-                if (clientInputCompletion != wordToExit)
+                switch (clientInput1)
                 {
-                    Console.Write("Какую валюту вы хотите обменять (RUB, EUR, USD): ");
-                    clientInput1 = Console.ReadLine();
-                    Console.Write("В какую валюту хотите сделать обмен: ");
-                    clientInput2 = Console.ReadLine();
+                    case CommandConversionCurrencyUSD:
+                        switch (clientInput2)
+                        {
+                            case CommandConversionCurrencyRUB:
+                                Console.Write("Сколько меняете " + clientInput1 + ": ");
+                                countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
 
-                    switch (clientInput1)
-                    {
-                        case nameCurrencyUSD:
-
-                            switch (clientInput2)
-                            {
-                                case nameCurrencyRUB:
-                                    Console.Write("Сколько меняете " + clientInput1 + ": ");
-                                    countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+                                if (clientUsd <= countСonversionsCurrency)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("!! Для данной конверции не хватает " +
+                                        (countСonversionsCurrency - clientUsd) + " " + clientInput1);
+                                }
+                                else
+                                {
                                     clientUsd -= countСonversionsCurrency;
                                     clientRub += countСonversionsCurrency * rubToUsd;
-                                    break;
-                                case nameCurrencyEUR:
-                                    Console.Write("Сколько меняете " + clientInput1 + ": ");
-                                    countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+                                }
+
+                                break;
+
+                            case CommandConversionCurrencyEUR:
+                                Console.Write("Сколько меняете " + clientInput1 + ": ");
+                                countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+
+                                if (clientUsd <= countСonversionsCurrency)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("!! Для данной конверции не хватает " +
+                                        (countСonversionsCurrency - clientUsd) + " " + clientInput1);
+                                }
+                                else
+                                {
                                     clientUsd -= countСonversionsCurrency;
                                     clientEur += countСonversionsCurrency * eurToUsd;
-                                    break;
-                                default:
-                                    Console.WriteLine("Мы не можем конвертировать в эту валюту! У нас такой нет!");
-                                    break;
-                            }
+                                }
 
-                            break;
-                        case nameCurrencyEUR:
+                                break;
 
-                            switch (clientInput2)
-                            {
-                                case nameCurrencyRUB:
-                                    Console.Write("Сколько меняете " + clientInput1 + ": ");
-                                    countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+                            default:
+                                Console.WriteLine("Мы не можем конвертировать в эту валюту! У нас такой нет!");
+                                break;
+                        }
+
+                        break;
+
+                    case CommandConversionCurrencyEUR:
+                        switch (clientInput2)
+                        {
+                            case CommandConversionCurrencyRUB:
+                                Console.Write("Сколько меняете " + clientInput1 + ": ");
+                                countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+
+                                if (clientEur <= countСonversionsCurrency)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("!! Для данной конверции не хватает " +
+                                        (countСonversionsCurrency - clientEur) + " " + clientInput1);
+                                }
+                                else
+                                {
                                     clientEur -= countСonversionsCurrency;
                                     clientRub += countСonversionsCurrency * rubToEur;
-                                    break;
-                                case nameCurrencyUSD:
-                                    Console.Write("Сколько меняете " + clientInput1 + ": ");
-                                    countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+                                }
+
+                                break;
+
+                            case CommandConversionCurrencyUSD:
+                                Console.Write("Сколько меняете " + clientInput1 + ": ");
+                                countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+
+                                if (clientEur <= countСonversionsCurrency)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("!! Для данной конверции не хватает " +
+                                        (countСonversionsCurrency - clientEur) + " " + clientInput1);
+                                }
+                                else
+                                {
                                     clientEur -= countСonversionsCurrency;
                                     clientUsd += countСonversionsCurrency * usdToEur;
-                                    break;
-                                default:
-                                    Console.WriteLine("Мы не можем конвертировать в эту валюту! У нас такой нет!");
-                                    break;
-                            }
+                                }
 
-                            break;
-                        case nameCurrencyRUB:
+                                break;
 
-                            switch (clientInput2)
-                            {
-                                case nameCurrencyUSD:
-                                    Console.Write("Сколько меняете " + clientInput1 + ": ");
-                                    countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+                            default:
+                                Console.WriteLine("Мы не можем конвертировать в эту валюту! У нас такой нет!");
+                                break;
+                        }
+
+                        break;
+
+                    case CommandConversionCurrencyRUB:
+                        switch (clientInput2)
+                        {
+                            case CommandConversionCurrencyUSD:
+                                Console.Write("Сколько меняете " + clientInput1 + ": ");
+                                countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+
+                                if (clientRub <= countСonversionsCurrency)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("!! Для данной конверции не хватает " +
+                                        (countСonversionsCurrency - clientRub) + " " + clientInput1);
+                                }
+                                else
+                                {
                                     clientRub -= countСonversionsCurrency;
                                     clientUsd += countСonversionsCurrency * usdToRub;
-                                    break;
-                                case nameCurrencyEUR:
-                                    Console.Write("Сколько меняете " + clientInput1 + ": ");
-                                    countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+                                }
+
+                                break;
+
+                            case CommandConversionCurrencyEUR:
+                                Console.Write("Сколько меняете " + clientInput1 + ": ");
+                                countСonversionsCurrency = Convert.ToInt32(Console.ReadLine());
+
+                                if (clientRub <= countСonversionsCurrency)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("!! Для данной конверции не хватает " +
+                                        (countСonversionsCurrency - clientRub) + " " + clientInput1);
+                                }
+                                else
+                                {
                                     clientRub -= countСonversionsCurrency;
                                     clientEur += countСonversionsCurrency * eurToRub;
-                                    break;
-                                default:
-                                    Console.WriteLine("Мы не можем конвертировать в эту валюту! У нас такой нет!");
-                                    break;
-                            }
+                                }
 
-                            break;
-                        default:
-                            Console.WriteLine("Мы не можем конвертировать эту валюту! У нас такой нет!");
-                            break;
-                    }
+                                break;
 
-                    Console.WriteLine("Баланс: " + clientRub + " Рублей; " + clientEur + " Евро; " + clientUsd + " Долларов;");
+                            default:
+                                Console.WriteLine("Мы не можем конвертировать в эту валюту! У нас такой нет!");
+                                break;
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Мы не можем конвертировать эту валюту! У нас такой нет!");
+                        break;
                 }
+
+                Console.WriteLine();
+                Console.WriteLine("Баланс: " + clientRub + " Рублей; " + clientEur + " Евро; " + clientUsd + " Долларов;");
+                Console.WriteLine();
+                Console.WriteLine("Для выхода введите - exit. Или нажмите Enter.");
+                clientInputCompletion = Console.ReadLine();
             }
         }
     }
