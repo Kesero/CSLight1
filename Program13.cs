685d507e38003b07e10706865e1f003563b985ec
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-15 20:48:44 +0100

    Add files via upload

--- Program13.cs
+++ Program13.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,10 +17,38 @@ namespace CSHomeWork1
             string symbolFraming = Console.ReadLine();
             string clientNameWithSideFrame = symbolFraming + clientName + symbolFraming;
             string frame = "";
+            string spacesBetweenFrameAndName = "";
 
             for (int i = 0; i < clientNameWithSideFrame.Length; i++)
             {
                 frame += symbolFraming;
+            }
+
+            if (symbolFraming.Length > 1)
+            {
+                bool presenceOddInName = false;
+
+                if (clientName.Length % 2 != 0)
+                {
+                    presenceOddInName = true;
+                }
+
+                int countSpacesBetweenFrameAndName = (frame.Length - clientNameWithSideFrame.Length) / 2;
+
+                for (int i = 0; i < countSpacesBetweenFrameAndName; i++)
+                {
+                    spacesBetweenFrameAndName += " ";
+                }
+
+                clientNameWithSideFrame = symbolFraming + spacesBetweenFrameAndName + 
+                    clientName + spacesBetweenFrameAndName;
+
+                if (presenceOddInName)
+                {
+                    clientNameWithSideFrame += " ";
+                }
+
+                clientNameWithSideFrame += symbolFraming;
             }
 
             Console.WriteLine(frame + "\n" + clientNameWithSideFrame + "\n" + frame);
