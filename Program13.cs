36b33a8499518c5b691c30e1ab86c3a02392bc14
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-16 18:51:12 +0100

    Add files via upload

--- Program13.cs
+++ Program13.cs
@@ -14,41 +14,13 @@ namespace CSHomeWork1
             Console.Write("Введите ваше имя: ");
             string clientName = Console.ReadLine();
             Console.Write("Введите значёк-обрамление вашего имени: ");
-            string symbolFraming = Console.ReadLine();
+            char symbolFraming = Console.ReadLine()[0];
             string clientNameWithSideFrame = symbolFraming + clientName + symbolFraming;
             string frame = "";
-            string spacesBetweenFrameAndName = "";
 
             for (int i = 0; i < clientNameWithSideFrame.Length; i++)
             {
                 frame += symbolFraming;
-            }
-
-            if (symbolFraming.Length > 1)
-            {
-                bool presenceOddInName = false;
-
-                if (clientName.Length % 2 != 0)
-                {
-                    presenceOddInName = true;
-                }
-
-                int countSpacesBetweenFrameAndName = (frame.Length - clientNameWithSideFrame.Length) / 2;
-
-                for (int i = 0; i < countSpacesBetweenFrameAndName; i++)
-                {
-                    spacesBetweenFrameAndName += " ";
-                }
-
-                clientNameWithSideFrame = symbolFraming + spacesBetweenFrameAndName + 
-                    clientName + spacesBetweenFrameAndName;
-
-                if (presenceOddInName)
-                {
-                    clientNameWithSideFrame += " ";
-                }
-
-                clientNameWithSideFrame += symbolFraming;
             }
 
             Console.WriteLine(frame + "\n" + clientNameWithSideFrame + "\n" + frame);
