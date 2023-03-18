744dab8fca6457182dff85c021d7706bebd6cb42
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-18 13:01:15 +0100

    Add files via upload

--- Program17.cs
+++ Program17.cs
@@ -13,59 +13,35 @@ namespace CSHomeWork1
         static void Main(string[] args)
         {
             String row = Console.ReadLine();
-            int countLeftBrackets = 0;
-            int countRightBrackets = 0;
-            bool isCorrectParenthesisExpression = false;
-            int depth;
+            int depth = 0;
             int maxDepth = 0;
+            bool isCorrectParenthesisExpression = true;
 
-            if (row.Length > 2 && row[0] != ')' && row[row.Length - 1] != '(')
+            foreach (var symbol in row)
             {
-                foreach (var symbol in row)
+                if (symbol == '(')
                 {
-                    if (symbol == '(')
-                    {
-                        countLeftBrackets++;
-                    } 
-                    else
-                    {
-                        countRightBrackets++;
-                    }
+                    depth++;
+                }
+                else
+                {
+                    depth--;
                 }
 
-                if (countLeftBrackets == countRightBrackets)
+                if (depth < 0)
                 {
-                    isCorrectParenthesisExpression |= true;
+                    isCorrectParenthesisExpression = false;
+                    break;
+                }
+
+                if (depth > maxDepth)
+                {
+                    maxDepth = depth;
                 }
             }
 
-            if (isCorrectParenthesisExpression)
+            if (isCorrectParenthesisExpression && depth == 0)
             {
-                for (int i1 = 0; i1 < row.Length - 1; i1++)
-                {
-                    if (row[i1] == '(' && row[i1 + 1] == ')')
-                    {
-                        depth = 1;
-
-                        for (int i2 = i1 + 2; i2 < row.Length; i2++)
-                        {
-                            if (row[i2] == '(')
-                            {
-                                depth--;
-                            }
-                            else
-                            {
-                                depth++;
-                            }
-                        }
-
-                        if (depth > maxDepth)
-                        {
-                            maxDepth = depth;
-                        }
-                    }
-                }
-
                 Console.WriteLine("Верное скобочное выражение");
                 Console.WriteLine("Глубина: " + maxDepth);
             }
@@ -73,7 +49,7 @@ namespace CSHomeWork1
             {
                 Console.WriteLine("Неверное скобочное выражение");
             }
-            
+
             Console.ReadKey();
         }
     }
