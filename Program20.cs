a9ced02d6c0cc769b87d9a8f61fc1b9d8265fd7f
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-21 20:31:54 +0100

    Add files via upload

---
+++ Program20.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countRows = 10;
+            int countCols = 10;
+            int minRAndomValue = 0;
+            int maxRAndomValue = 10;
+            int numberHighest = int.MinValue;
+            int numberReplacementOfLargest = 0;
+            int[,] array = new int[countRows, countCols];
+            Random random = new Random();
+
+            for (int i1 = 0; i1 < array.GetLength(0); i1++)
+            {
+                for (int i2 = 0; i2 < array.GetLength(1); i2++)
+                {
+                    array[i1, i2] = random.Next(minRAndomValue, maxRAndomValue);
+                    Console.Write(array[i1, i2] + " ");
+
+                    if (array[i1, i2] > numberHighest)
+                    {
+                        numberHighest = array[i1, i2];
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Максимальный элемент матрицы: " + numberHighest);
+
+            for (int i1 = 0; i1 < array.GetLength(0); i1++)
+            {
+                for (int i2 = 0; i2 < array.GetLength(1); i2++)
+                {
+                    if (array[i1, i2] == numberHighest)
+                    {
+                        array[i1, i2] = numberReplacementOfLargest;
+                    }
+
+                    Console.Write(array[i1, i2] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
