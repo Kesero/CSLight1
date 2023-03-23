38eac76a00490e0487bbf45cb7025e808f6cb653
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-23 19:29:19 +0100

    Add files via upload

---
+++ Program23.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = 30;
+            int maxRandomValue = 10;
+            Random random = new Random();
+            int[] array = new int[arrayLength];
+            int countMaxRepeat = int.MinValue;
+            int countRepeat = 1;
+            int repeatNumber = 0;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(maxRandomValue);
+                Console.Write(array[i] + " ");
+
+                if (i != 0 && array[i] == array[i - 1])
+                {
+                    countRepeat++;
+                    if (countMaxRepeat < countRepeat)
+                    {
+                        countMaxRepeat = countRepeat;
+                        repeatNumber = array[i];
+                    }
+                    repeatNumber = array[i];
+                }
+                else
+                {
+                    countRepeat = 1;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Наибольшеей кол-во повторений: {countMaxRepeat}\nПовторяющееся максимальное кол-во раз число: {repeatNumber}");
+            Console.ReadKey();
+        }
+    }
+}
