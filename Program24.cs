28468d4855cae0d83656615cd24986cfdb56c189
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-24 19:49:22 +0100

    Add files via upload

---
+++ Program24.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSHomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = 30;
+            int maxRandomValue = 10;
+            Random random = new Random();
+            int[] numbers = new int[arrayLength];
+            Console.WriteLine("Начальный массив: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(maxRandomValue);
+                Console.Write(numbers[i] + " ");
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int copyToMove = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = copyToMove;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Конечный массив: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
