5e4aa939940e61130f9c8d3e8dfe9014186831e3
Kesero <127146733+Kesero@users.noreply.github.com>
2023-03-24 19:54:39 +0100

    Add files via upload

---
+++ Program25.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSHomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] textWordByWord = text.Split(' ');
+            Console.WriteLine();
+
+            for (int i = 0; i < textWordByWord.Length; i++)
+            {
+                Console.WriteLine(textWordByWord[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
