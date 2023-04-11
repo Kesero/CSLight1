2b2491b96898f3b43e95b2c3990fce47ddab715a
Kesero <127146733+Kesero@users.noreply.github.com>
2023-04-11 19:41:48 +0200

    Add files via upload

---
+++ Program28.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSHomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientInput;
+
+            do
+            {
+                Console.Write("Введите число: ");
+                clientInput = Console.ReadLine();
+            }
+            while (!int.TryParse(clientInput, out int resaultOfParse));
+
+            Console.WriteLine("Введённое число: " + clientInput);
+        }
+    }
+}
