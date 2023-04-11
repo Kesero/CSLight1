3988020eac57c1ea1aa86ffe99e69666030dc016
Kesero <127146733+Kesero@users.noreply.github.com>
2023-04-11 19:31:39 +0200

    Add files via upload

--- Program27.cs
+++ Program27.cs
@@ -6,28 +6,42 @@ namespace CSHomeWork1
     {
         static void Main(string[] args)
         {
-            int percentageOccupancyBar = 0;
+            string clientInput = "";
+            string commandExit = "-0";
             int maxBarValue = 10;
+            double percentageRatio = 100.0;
+            int percentageOccupancyBar;
+            int barValue;
 
-            while (true)
+            while (clientInput != commandExit)
             {
-                int barValue = Convert.ToInt32(percentageOccupancyBar / 100.0 * maxBarValue);
+                Console.Write("Какой процент здоровья отобразить: ");
+                clientInput = Console.ReadLine();
+                percentageOccupancyBar = Convert.ToInt32(clientInput);
+                barValue = Convert.ToInt32(percentageOccupancyBar / percentageRatio * maxBarValue);
                 Console.Clear();
                 DrawBar(0, 10, "Health", ' ', ConsoleColor.Red, maxBarValue, barValue);
                 Console.SetCursorPosition(0, 0);
-                Console.Write("Какой процент здоровья отобразить: ");
-                percentageOccupancyBar = Convert.ToInt32(Console.ReadLine());
             }
         }
 
-        static void DrawBar(int x, int y, string barName, char symbol, ConsoleColor color, int maxValue, int value)
+        static void DrawBar(int xCoordinateOfConsole, int yCoordinateOfConsole, string barName, char symbol, ConsoleColor color, int maxValue, int value)
         {
             string bar = "";
             char voidSymbol = ' ';
             ConsoleColor defaultColor = Console.BackgroundColor;
-            Console.SetCursorPosition(x, y);
+            Console.SetCursorPosition(xCoordinateOfConsole, yCoordinateOfConsole);
             Console.Write(barName + " [");
             Console.BackgroundColor = color;
+
+            if (value >= maxValue)
+            {
+                value = maxValue;
+            }
+            else if (value < 0)
+            {
+                value = 0;
+            }
 
             for (int i = 0; i < value; i++)
             {
