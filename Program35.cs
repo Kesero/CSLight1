30dcc3579ccc8e9fdb87d5efb5c1d4bf7a047b36
Kesero <127146733+Kesero@users.noreply.github.com>
2023-06-12 15:26:15 +0200

    Add files via upload

---
+++ Program35.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace CSHomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(10, 10, "Marek", 0);
+            player1.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string Name { get; private set; }
+        public int Score { get; private set; }
+
+        public Player (int x, int y, string name, int score)
+        {
+            X = x;
+            Y = y;
+            Name = name;
+            Score = score;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Игрок - {Name}\nРасположение - X:{X} Y:{Y}\nСчёт - {Score}");
+        }
+    }
+}
